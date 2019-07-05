11df806b81a77547d157c0c18e3fa23c3b5da234
Solomon Feldman <solomonrfeldman@gmail.com>
2019-07-05 15:50:29 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -30,7 +30,7 @@ def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_
   INNER JOIN pledges 
   ON projects.id = pledges.project_id
   GROUP BY(pledges.project_id)
-  
+  HAVING past_funding >= 0
   "
   
 end
